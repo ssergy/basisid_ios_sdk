c2a376a7650531d41a3ffc2f562f28080501c5f3
ssergy <sergysh@gmail.com>
2021-10-11 23:29:20 +0300

    BasisIDSdk.xcframework

---
+++ BasisIDSdk.xcframework/ios-arm64/BasisIDSdk.framework/Headers/BasisIDSdk.h
@@ -0,0 +1,18 @@
+//
+//  BasisIDSdk.h
+//  BasisIDSdk
+//
+//  Created by Sergey Shcherbak on 10.10.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BasisIDSdk.
+FOUNDATION_EXPORT double BasisIDSdkVersionNumber;
+
+//! Project version string for BasisIDSdk.
+FOUNDATION_EXPORT const unsigned char BasisIDSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasisIDSdk/PublicHeader.h>
+
+
